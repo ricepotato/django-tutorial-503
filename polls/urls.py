06526431ebf37250e3db1a878a8578fc3ec3f4bf
ricepotato <ricepotato40@gmail.com>
2024-03-22 23:12:22 +0900

    feat: part4

--- polls/urls.py
+++ polls/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from . import views
 
-app_name = "polls" # namespace
+app_name = "polls"  # namespace
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("<int:question_id>/", views.detail, name="detail"),
-    path("<int:question_id>/results/", views.results, name="results"),
+    path("", views.IndexView.as_view(), name="index"),
+    # path("", views.index, name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    # path("<int:question_id>/", views.detail, name="detail"),
+    path("<int:pk>/results/", views.ResultView.as_view(), name="results"),
+    # path("<int:question_id>/results/", views.results, name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.db.models import F
+from django.db.models.query import QuerySet
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.template import loader
 from django.urls import reverse
+from django.views import generic
 
 
 from . import models
@@ -49,3 +51,21 @@ def vote(request, question_id):
         selected_choice.votes = F("votes") + 1
         selected_choice.save()
         return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+
+
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        return models.Question.objects.order_by("-pub_date")[:5]
+
+
+class DetailView(generic.DetailView):
+    model = models.Question
+    template_name = "polls/detail.html"
+
+
+class ResultView(generic.DetailView):
+    model = models.Question
+    template_name = "polls/result.html"
